24973b923c737f993efc662d85caf01c2310482a
DK-PRIME <d.n.i.o.n.i.r@gmail.com>
2025-12-05 20:23:18 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBU7BSwGl0laDvHGhrvu14nJWpabsjSoNo",
-  authDomain: "stolar-carp.firebaseapp.com",
-  projectId: "stolar-carp",
-  storageBucket: "stolar-carp.firebasestorage.app",
-  messagingSenderId: "1019636788370",
-  appId: "1:1019636788370:web:af1c1ecadb683df212ca4b"
+  // ... (Ваша конфігурація)
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// щоб можна було глянути в консолі браузера
-window._scTest = { app, auth, db };
+export { auth, db, app };
