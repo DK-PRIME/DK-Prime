ba7cc4e5b0b61b1a2a1f2a76912eabfa0b4f1298
DK-PRIME <d.n.i.o.n.i.r@gmail.com>
2025-12-07 12:18:53 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth }       from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+// firebase-config.js
 
+// ЄДИНИЙ Firebase-конфіг для всього сайту
+// Використовуємо одну версію SDK: 11.0.1
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth }       from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore }  from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+// ТВОЯ реальна конфігурація проєкту STOLAR CARP
 const firebaseConfig = {
   apiKey: "AIzaSyBU7BSwGl0laDvHGhrvu14nJWpabsjSoNo",
   authDomain: "stolar-carp.firebaseapp.com",
@@ -12,6 +18,9 @@ const firebaseConfig = {
   measurementId: "G-VWC07QNS7P"
 };
 
+// Ініціалізація додатку (робимо це ТІЛЬКИ тут)
 export const app  = initializeApp(firebaseConfig);
+
+// Спільні екземпляри auth і db для всіх інших модулів
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
